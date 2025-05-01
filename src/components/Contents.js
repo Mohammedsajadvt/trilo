99e0ce15524a56557352c2a55a1cf51f63725f0c
Mohammed Sajad VT <mohammedsajadvt@gmail.com>
2025-05-01 17:15:38 +0530

    Header section

---
+++ src/components/Contents.js
@@ -0,0 +1,12 @@
+function Contents() {
+    return (
+        <div>
+            <form className="form">
+               <input></input>
+               <input></input>
+            </form>
+        </div>
+    );
+}
+
+export default Contents;
